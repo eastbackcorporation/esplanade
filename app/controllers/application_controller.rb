37f8bc6cc340de2cfd947c12862980b2b7417be0
Takahiro Seii <seiitakahiro@gmail.com>
2015-11-30 11:40:02 +0900

    fix after sign in path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  after_filter  :store_location
 
 protected
   def configure_permitted_parameters
@@ -11,6 +12,22 @@ protected
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
   end
 
+  def store_location
+    if (request.fullpath != "/users/sign_in" &&
+        request.fullpath != "/users/sign_up" &&
+        request.fullpath !~ Regexp.new("\\A/users/password.*\\z") &&
+        !request.xhr?)
+      session[:previous_url] = request.fullpath
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    if (session[:previous_url] == root_path)
+      super
+    else
+      session[:previous_url] || root_path
+    end
+  end
 private
   def sign_in_required
     redirect_to new_user_session_url unless user_signed_in?

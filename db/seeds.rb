331312db0b4f39cd6df3b0139e43e572ed10038a
Takahiro Seii <seiitakahiro@gmail.com>
2015-12-07 00:13:15 +0900

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,23 +17,25 @@ User.find_or_create_by(id: 2, email: "user1@example.com") do |u|
                         u.password_confirmation = "pass1234"
                         u.username = "username1"
 end
+ActiveRecord::Base.connection.execute("SELECT setval('users_id_seq', coalesce((SELECT MAX(id)+1 FROM users), 1), false)")
 
 Category.find_or_create_by(id: 1,title: "category1")
 Category.find_or_create_by(id: 2,title: "category2"){|c| c.image = open "test/image/200x300.png"}
 Category.find_or_create_by(id: 3,title: "none topic category")
+ActiveRecord::Base.connection.execute("SELECT setval('categories_id_seq', coalesce((SELECT MAX(id)+1 FROM categories), 1), false)")
 
 (1..10).each do |i|
-  Topic.find_or_create_by(id: i,title: "topic #{i}",value: "value #{i}",category_id: 1,user_id: i%2+1)
+  Topic.find_or_create_by(title: "topic #{i}",value: "value #{i}",category_id: 1,user_id: i%2+1)
 end
 
 (11..15).each do |i|
-  Topic.find_or_create_by(id: i,title: "topic #{i}",value: "value #{i}",category_id: 2,user_id: 1)
+  Topic.find_or_create_by(title: "topic #{i}",value: "value #{i}",category_id: 2,user_id: 1)
 end
 
 (1..10).each do |i|
-  Comment.find_or_create_by(id: i,value: "comment #{i}",topic_id: 1,user_id: i%2+1)
+  Comment.find_or_create_by(value: "comment #{i}",topic_id: 1,user_id: i%2+1)
 end
 (11..15).each do |i|
-  Comment.find_or_create_by(id: i,value: "comment #{i}",topic_id: 11,user_id: 1){|c| c.image = open "test/image/200x300.png"}
+  Comment.find_or_create_by(value: "comment #{i}",topic_id: 11,user_id: 1){|c| c.image = open "test/image/200x300.png"}
 end
 

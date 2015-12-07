2365954e42e4fc8ad38b0a0d1c3e2afb8808fdb9
Takahiro Seii <seiitakahiro@gmail.com>
2015-12-07 16:02:16 +0900

    fix view(users/show)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @topics = Topic.where(user_id: @user.id)
+    @comments = Comment.where(user_id: @user.id)
   end
 end

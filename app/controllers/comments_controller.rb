e2665cfd2c73fe21aa0c39dc673cfc6f787dabef
Takahiro Seii <seiitakahiro@gmail.com>
2015-12-18 09:11:25 +0900

    fix view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,6 +24,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.user_id = current_user.id
+    @comment.topic.touch
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @comment.topic, notice: "コメントを作成しました" }

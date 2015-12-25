7dfe95f19ac62a6abbcd7283b0f80537a203409c
Takahiro Seii <seiitakahiro@gmail.com>
2015-12-25 17:08:03 +0900

    fix category status

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :topics
   mount_uploader :image, ImageUploader
-  enum status: [:active, :archived, :deleted]
+  enum status: [:active, :deleted]
 end

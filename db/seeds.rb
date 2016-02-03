eeff341478ac7e10ece02442a99f7a16ae3f2aa4
Takahiro Seii <seiitakahiro@gmail.com>
2016-02-04 02:18:39 +0900

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-require_relative 'demo_data/users'
-require_relative 'demo_data/categories'
-require_relative 'demo_data/topics'
-require_relative 'demo_data/comments'
+# admin
+User.find_or_create_by(id: 1,email: "admin1@example.com") do |u|
+                        u.password = "admin1234"
+                        u.password_confirmation = "admin1234"
+                        u.username = "AdminName1"
+                        u.admin = true
+end
